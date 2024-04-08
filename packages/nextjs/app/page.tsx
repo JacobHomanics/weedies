754eeb4f95518fba4de1c242926f69d5905d1af5
Jacob Homanics <homanicsjake@gmail.com>
2024-04-07 20:31:40 -0500

    added latest mock

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -3,12 +3,21 @@
 import { useCallback, useEffect } from "react";
 import { useMemo } from "react";
 import { useState } from "react";
+import nounie1 from "../public/carousel/1.png";
+import nounie2 from "../public/carousel/2.png";
+import nounie3 from "../public/carousel/3.png";
+import nounie4 from "../public/carousel/4.png";
+// import nounie5 from "../public/carousel/5.png";
+import HeroImageCharacter from "../public/weedies-samples/Hero-Image-characters.png";
+import NouniesLogo from "../public/weedies-samples/Nounies_logo.png";
 import type { NextPage } from "next";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import { formatEther } from "viem";
 // import { useAccount } from "wagmi";
-import { useBalance } from "wagmi";
-import { NftCard } from "~~/components/NftCard";
-import { Address } from "~~/components/scaffold-eth";
+// import { useBalance } from "wagmi";
+// import { NftCard } from "~~/components/NftCard";
+// import { Address } from "~~/components/scaffold-eth";
 import { useScaffoldContract, useScaffoldContractRead, useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 function useTokenIds(numOfTokens: number) {
@@ -69,41 +78,41 @@ function useUris(contract: any, tokenIds: bigint[]) {
   return { uris, setUris, refetch };
 }
 
-function useFetches(uris: string[]) {
-  const [responses, setResponses] = useState<any[]>([]);
+// function useFetches(uris: string[]) {
+//   const [responses, setResponses] = useState<any[]>([]);
 
-  const refetch = useCallback(async () => {
-    const arr = [];
-    for (let i = 0; i < uris.length; i++) {
-      const response = await fetch(uris[i]);
-      const responseJson = await response.json();
-      arr.push(responseJson);
-    }
+//   const refetch = useCallback(async () => {
+//     const arr = [];
+//     for (let i = 0; i < uris.length; i++) {
+//       const response = await fetch(uris[i]);
+//       const responseJson = await response.json();
+//       arr.push(responseJson);
+//     }
 
-    setResponses([...arr]);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [uris.length]);
+//     setResponses([...arr]);
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [uris.length]);
 
-  useEffect(() => {
-    async function get() {
-      await refetch();
-    }
+//   useEffect(() => {
+//     async function get() {
+//       await refetch();
+//     }
 
-    get();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [uris.length, refetch]);
+//     get();
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [uris.length, refetch]);
 
-  return { responses, refetch };
-}
+//   return { responses, refetch };
+// }
 
 const Home: NextPage = () => {
   // const { address: connectedAddress } = useAccount();
   const { writeAsync: mint } = useScaffoldContractWrite({ contractName: "YourContract", functionName: "mint" });
-  const { writeAsync: withdraw } = useScaffoldContractWrite({ contractName: "YourContract", functionName: "withdraw" });
-  const { writeAsync: startMint } = useScaffoldContractWrite({
-    contractName: "YourContract",
-    functionName: "startMint",
-  });
+  // const { writeAsync: withdraw } = useScaffoldContractWrite({ contractName: "YourContract", functionName: "withdraw" });
+  // const { writeAsync: startMint } = useScaffoldContractWrite({
+  //   contractName: "YourContract",
+  //   functionName: "startMint",
+  // });
 
   const { data: mintCount, refetch: refetchMintCount } = useScaffoldContractRead({
     contractName: "YourContract",
@@ -115,35 +124,35 @@ const Home: NextPage = () => {
     functionName: "getStartMintTimestamp",
   });
 
-  const { data: isMintStarted, refetch: refetchIsMintStarted } = useScaffoldContractRead({
-    contractName: "YourContract",
-    functionName: "getIsMintStarted",
-  });
+  // const { data: isMintStarted, refetch: refetchIsMintStarted } = useScaffoldContractRead({
+  //   contractName: "YourContract",
+  //   functionName: "getIsMintStarted",
+  // });
 
   const { data: mintDuration, refetch: refetchGetWindow } = useScaffoldContractRead({
     contractName: "YourContract",
     functionName: "getMintDuration",
   });
 
-  const { data: maxMintCount } = useScaffoldContractRead({
-    contractName: "YourContract",
-    functionName: "getMaxMintCount",
-  });
+  // const { data: maxMintCount } = useScaffoldContractRead({
+  //   contractName: "YourContract",
+  //   functionName: "getMaxMintCount",
+  // });
 
-  const { data: tokenName } = useScaffoldContractRead({
-    contractName: "YourContract",
-    functionName: "name",
-  });
+  // const { data: tokenName } = useScaffoldContractRead({
+  //   contractName: "YourContract",
+  //   functionName: "name",
+  // });
 
-  const { data: tokenSymbol } = useScaffoldContractRead({
-    contractName: "YourContract",
-    functionName: "symbol",
-  });
+  // const { data: tokenSymbol } = useScaffoldContractRead({
+  //   contractName: "YourContract",
+  //   functionName: "symbol",
+  // });
 
-  const { data: royaltyRecipient } = useScaffoldContractRead({
-    contractName: "YourContract",
-    functionName: "getRoyaltyRecipient",
-  });
+  // const { data: royaltyRecipient } = useScaffoldContractRead({
+  //   contractName: "YourContract",
+  //   functionName: "getRoyaltyRecipient",
+  // });
 
   const { data: activeThreshold, refetch: refetchActiveThrehsold } = useScaffoldContractRead({
     contractName: "YourContract",
@@ -151,7 +160,7 @@ const Home: NextPage = () => {
   });
 
   const { data: yourContract } = useScaffoldContract({ contractName: "YourContract" });
-  const { data: balance, refetch: refetchBalance } = useBalance({ address: yourContract?.address });
+  // const { data: balance, refetch: refetchBalance } = useBalance({ address: yourContract?.address });
 
   const { tokenIds, refetch: refetchTokenIds } = useTokenIds(Number(mintCount));
 
@@ -161,11 +170,11 @@ const Home: NextPage = () => {
     uris[i] = uris[i].replace("https://nft.bueno.art", "https://app.bueno.art");
   }
 
-  const { responses, refetch: refetchResponses } = useFetches(uris);
+  // const { responses, refetch: refetchResponses } = useFetches(uris);
 
-  const allNfts = responses.map((response, index) => {
-    return <NftCard key={index} data={response} />;
-  });
+  // const allNfts = responses.map((response, index) => {
+  //   return <NftCard key={index} data={response} />;
+  // });
 
   const date = new Date(Number(startMintTimestamp) * 1000);
   const endDate = new Date((startMintTimestamp && mintDuration ? Number(startMintTimestamp + mintDuration) : 0) * 1000);
@@ -186,25 +195,166 @@ const Home: NextPage = () => {
     minute: "2-digit",
   });
 
-  function secondsToDhms(seconds: number) {
-    seconds = Number(seconds);
-    const d = Math.floor(seconds / (3600 * 24));
-    const h = Math.floor((seconds % (3600 * 24)) / 3600);
-    const m = Math.floor((seconds % 3600) / 60);
-    const s = Math.floor(seconds % 60);
+  // function secondsToDhms(seconds: number) {
+  //   seconds = Number(seconds);
+  //   const d = Math.floor(seconds / (3600 * 24));
+  //   const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  //   const m = Math.floor((seconds % 3600) / 60);
+  //   const s = Math.floor(seconds % 60);
 
-    const dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
-    const hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
-    const mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
-    const sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-    return (dDisplay + hDisplay + mDisplay + sDisplay).replace(/,\s*$/, "");
-  }
+  //   const dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+  //   const hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+  //   const mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+  //   const sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+  //   return (dDisplay + hDisplay + mDisplay + sDisplay).replace(/,\s*$/, "");
+  // }
 
-  const mintDurationFormatted = secondsToDhms(Number(mintDuration));
+  // const mintDurationFormatted = secondsToDhms(Number(mintDuration));
 
   return (
     <>
+      <div className="relative">
+        <img src={NouniesLogo.src} className="absolute inset-y-5" />
+
+        <img src={HeroImageCharacter.src} />
+      </div>
+      <div className="flex items-center justify-center text-center">
+        <p className="font-nouns font-black text-xl w-80 -mt-20 lg:text-6xl lg:w-7/12 lg:-mt-60">
+          Nounies are Dreamers, Rebels, Creators, Artists, and Friends living their best life on the blockchain.
+        </p>
+      </div>
+
       <div className="flex items-center flex-col flex-grow pt-10 bg-base-100">
+        <Carousel
+          additionalTransfrom={0}
+          arrows
+          autoPlay
+          autoPlaySpeed={1000}
+          centerMode
+          className=""
+          containerClass="container"
+          dotListClass=""
+          draggable
+          focusOnSelect={false}
+          infinite
+          itemClass=""
+          keyBoardControl
+          minimumTouchDrag={80}
+          pauseOnHover
+          renderArrowsWhenDisabled={false}
+          renderButtonGroupOutside={false}
+          renderDotsOutside={false}
+          responsive={{
+            desktop: {
+              breakpoint: {
+                max: 3000,
+                min: 1024,
+              },
+              items: 1,
+              partialVisibilityGutter: 30,
+            },
+            mobile: {
+              breakpoint: {
+                max: 464,
+                min: 0,
+              },
+              items: 1,
+              partialVisibilityGutter: 20,
+            },
+            tablet: {
+              breakpoint: {
+                max: 1024,
+                min: 464,
+              },
+              items: 1,
+              partialVisibilityGutter: 20,
+            },
+          }}
+          rewind={false}
+          rewindWithAnimation={false}
+          rtl={false}
+          shouldResetAutoplay
+          showDots={false}
+          sliderClass=""
+          slidesToSlide={1}
+          swipeable
+        >
+          <div>
+            <img src={nounie1.src} alt="Fruits" />
+          </div>
+
+          <div>
+            <img src={nounie2.src} alt="Fruits" />
+          </div>
+
+          <div>
+            <img src={nounie3.src} alt="Fruits" />
+          </div>
+
+          <div>
+            <img src={nounie4.src} alt="Fruits" />
+          </div>
+        </Carousel>
+        <div className="flex items-center justify-center text-center">
+          <p className="font-nouns font-black text-2xl">Swipe around, find out.</p>
+        </div>
+
+        <div className="flex flex-col items-center justify-center text-center">
+          <p className="font-nouns font-black text-4xl">Mint a Weedie</p>
+        </div>
+
+        <div className="flex flex-wrap justify-center">
+          <div className="flex flex-col text-center bg-base-200 rounded-lg p-2 w-40 m-1">
+            <p className="font-nouns font-black text-lg m-0">Mint Price</p>
+            <p className="text-md m-0">{formatEther(activeThreshold?.mintPrice || BigInt(0)).toString()}</p>
+          </div>
+          <div className="flex flex-col text-center bg-base-200 rounded-lg p-2 w-40 m-1">
+            <p className="font-nouns font-black text-lg m-0">Starts</p>
+            <p className="text-sm m-0">{startDateLocale}</p>
+          </div>
+          <div className="flex flex-col text-center bg-base-200 rounded-lg p-2 w-40 m-1">
+            <p className="font-nouns font-black text-lg m-0">Ends</p>
+            <p className="text-sm m-0">{endDateLocale}</p>
+          </div>
+        </div>
+
+        <button
+          onClick={async () => {
+            await mint({ value: activeThreshold?.mintPrice });
+            await refetchMintCount();
+            await refetchActiveThrehsold();
+            // await refetchBalance();
+            await refetchTokenIds();
+            await refetchUris();
+            // await refetchResponses();
+            await refetchStartMintTimestamp();
+            // await refetchIsMintStarted();
+            await refetchGetWindow();
+          }}
+          className="btn btn-secondary btn-lg"
+        >
+          Mint
+        </button>
+
+        {/* <button
+          onClick={async () => {
+            await startMint();
+            await refetchMintCount();
+            await refetchActiveThrehsold();
+            await refetchBalance();
+            await refetchTokenIds();
+            await refetchUris();
+            await refetchResponses();
+            await refetchIsMintStarted();
+            await refetchGetWindow();
+          }}
+          className="btn btn-secondary btn-sm"
+        >
+          Start Mint
+        </button>
+
+        <div className="flex flex-col items-center justify-center text-center"></div>
+
         <div className="px-5 items-center text-center">
           <button
             onClick={async () => {
@@ -223,23 +373,6 @@ const Home: NextPage = () => {
             Start Mint
           </button>
 
-          <button
-            onClick={async () => {
-              await mint({ value: activeThreshold?.mintPrice });
-              await refetchMintCount();
-              await refetchActiveThrehsold();
-              await refetchBalance();
-              await refetchTokenIds();
-              await refetchUris();
-              await refetchResponses();
-              await refetchStartMintTimestamp();
-              await refetchIsMintStarted();
-              await refetchGetWindow();
-            }}
-            className="btn btn-secondary btn-sm"
-          >
-            Mint
-          </button>
 
           <button
             onClick={async () => {
@@ -314,7 +447,7 @@ const Home: NextPage = () => {
 
           <p className="text-center text-2xl">All Weedies</p>
           <div className="flex flex-wrap justify-center bg-base-100 rounded-lg m-4">{allNfts}</div>
-        </div>
+        </div> */}
       </div>
     </>
   );
--- packages/nextjs/tailwind.config.js
+++ packages/nextjs/tailwind.config.js
@@ -75,6 +75,9 @@ module.exports = {
   },
   theme: {
     extend: {
+      fontFamily: {
+        nouns: ["Londrina-Solid"],
+      },
       boxShadow: {
         center: "0 0 12px -2px rgb(0 0 0 / 0.05)",
       },
