b2572f2ab3458e0196a062756570220342c7a310
Jacob Homanics <homanicsjake@gmail.com>
2024-04-09 17:33:47 -0500

    reconfigured deploy script

--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -7,6 +7,41 @@ import "./DeployHelpers.s.sol";
 contract DeployScript is ScaffoldETHDeploy {
     error InvalidPrivateKey(string);
 
+    function getSetup()
+        public
+        view
+        returns (
+            uint256 mintStartTimestamp,
+            uint256 mintEndTimestamp,
+            YourContract.MintingThreshold[] memory thresholds
+        )
+    {
+        uint256 chainId;
+        assembly {
+            chainId := chainid()
+        }
+
+        if (chainId == 31337) {
+            mintStartTimestamp = 1 minutes;
+            mintEndTimestamp = 1 days;
+
+            thresholds = new YourContract.MintingThreshold[](2);
+            thresholds[0] = YourContract.MintingThreshold(0, 3, 0 ether);
+            thresholds[1] = YourContract.MintingThreshold(
+                1000, type(uint256).max, 0.0006942 ether
+            );
+        } else if (chainId == 8453) {
+            mintStartTimestamp = 1713589200;
+            mintEndTimestamp = 1 days;
+
+            thresholds = new YourContract.MintingThreshold[](2);
+            thresholds[0] = YourContract.MintingThreshold(0, 1000, 0);
+            thresholds[1] = YourContract.MintingThreshold(
+                1000, type(uint256).max, 0.0006942 ether
+            );
+        }
+    }
+
     function run() external {
         uint256 deployerPrivateKey = setupLocalhostEnv();
         if (deployerPrivateKey == 0) {
@@ -15,26 +50,26 @@ contract DeployScript is ScaffoldETHDeploy {
             );
         }
 
-        // YourContract.MintingThreshold[] memory thresholds =
-        //     new YourContract.MintingThreshold[](2);
-        // thresholds[0] = YourContract.MintingThreshold(0, 1000, 0 ether);
-        // thresholds[1] = YourContract.MintingThreshold(
-        //     1000, type(uint256).max, 0.0006942 ether
-        // );
+        (
+            uint256 mintStartTimestamp,
+            uint256 mintEndTimestamp,
+            YourContract.MintingThreshold[] memory thresholds
+        ) = getSetup();
 
-        // vm.startBroadcast(deployerPrivateKey);
-        // YourContract yourContract = new YourContract(
-        //     0x2F15D4A66D22ecC6967928b6A76Ab06897b05676, //0x4161f8A8DfF60aEdB63baFb7d5843b0988393eC9,
-        //     0x2F15D4A66D22ecC6967928b6A76Ab06897b05676, //0x4161f8A8DfF60aEdB63baFb7d5843b0988393eC9,
-        //     24420,
-        //     24 hours,
-        //     thresholds
-        // );
-        // console.logString(
-        //     string.concat(
-        //         "YourContract deployed at: ", vm.toString(address(yourContract))
-        //     )
-        // );
+        vm.startBroadcast(deployerPrivateKey);
+        YourContract yourContract = new YourContract(
+            0x2F15D4A66D22ecC6967928b6A76Ab06897b05676, //0x4161f8A8DfF60aEdB63baFb7d5843b0988393eC9,
+            "https://nft.bueno.art/api/contract/0zJlzGVsEKj7cALqS-QMX/chain/1/metadata/",
+            24420,
+            mintStartTimestamp,
+            mintEndTimestamp,
+            thresholds
+        );
+        console.logString(
+            string.concat(
+                "YourContract deployed at: ", vm.toString(address(yourContract))
+            )
+        );
         vm.stopBroadcast();
 
         /**
