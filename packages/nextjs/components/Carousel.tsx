0a672f70dd315b086fb1d6c50da84b5716e2c8ce
Jacob Homanics <homanicsjake@gmail.com>
2024-04-11 21:31:11 -0500

    weedies logo and carousel images updated

--- packages/nextjs/components/Carousel.tsx
+++ packages/nextjs/components/Carousel.tsx
@@ -1,64 +1,78 @@
 import { useEffect, useState } from "react";
-import carouselImage2 from "../public/images-carousel/2.png";
-import carouselImage3 from "../public/images-carousel/3.png";
-import carouselImage4 from "../public/images-carousel/4.png";
-import carouselImage5 from "../public/images-carousel/5.png";
+// import carouselImage2 from "../public/images-carousel/2.png";
+// import carouselImage3 from "../public/images-carousel/3.png";
+// import carouselImage4 from "../public/images-carousel/4.png";
+// import carouselImage5 from "../public/images-carousel/5.png";
 import Card from "./Card";
 import { config } from "react-spring";
 import Carousel from "react-spring-3d-carousel";
 
-const staticCards = [
-  {
-    key: 1,
-    content: (
-      <Card
-        imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/convertplus_thumbnail.jpg"
-        image={carouselImage2.src}
-      />
-    ),
-  },
-  {
-    key: 2,
-    content: (
-      <Card
-        imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/acf_pro.png"
-        image={carouselImage2.src}
-      />
-    ),
-  },
-  {
-    key: 3,
-    content: (
-      <Card
-        imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/layer_slider_plugin_thumb.png"
-        image={carouselImage3.src}
-      />
-    ),
-  },
-  {
-    key: 4,
-    content: (
-      <Card
-        imagen="https://updates.theme-fusion.com/wp-content/uploads/2016/08/slider_revolution-1.png"
-        image={carouselImage4.src}
-      />
-    ),
-  },
-  {
-    key: 5,
-    content: (
-      <Card
-        imagen="https://updates.theme-fusion.com/wp-content/uploads/2016/08/slider_revolution-1.png"
-        image={carouselImage5.src}
-      />
-    ),
-  },
-];
+// const staticCards = [
+//   {
+//     key: 1,
+//     content: (
+//       <Card
+//         imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/convertplus_thumbnail.jpg"
+//         image={carouselImage2.src}
+//       />
+//     ),
+//   },
+//   {
+//     key: 2,
+//     content: (
+//       <Card
+//         imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/acf_pro.png"
+//         image={carouselImage2.src}
+//       />
+//     ),
+//   },
+//   {
+//     key: 3,
+//     content: (
+//       <Card
+//         imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/layer_slider_plugin_thumb.png"
+//         image={carouselImage3.src}
+//       />
+//     ),
+//   },
+//   {
+//     key: 4,
+//     content: (
+//       <Card
+//         imagen="https://updates.theme-fusion.com/wp-content/uploads/2016/08/slider_revolution-1.png"
+//         image={carouselImage4.src}
+//       />
+//     ),
+//   },
+//   {
+//     key: 5,
+//     content: (
+//       <Card
+//         imagen="https://updates.theme-fusion.com/wp-content/uploads/2016/08/slider_revolution-1.png"
+//         image={carouselImage5.src}
+//       />
+//     ),
+//   },
+// ];
 
 export default function Carroussel() {
+  const staticCards2: any = [];
+
+  for (let i = 1; i <= 23; i++) {
+    staticCards2.push({
+      key: i,
+      content: (
+        <Card
+          imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/convertplus_thumbnail.jpg"
+          image={"/images-carousel/" + i + ".png"}
+        />
+      ),
+    });
+  }
+
   const offset = 2;
 
-  const table = staticCards.map((element: any, index: any) => {
+  const table = staticCards2.map((element: any, index: any) => {
     return { ...element, onClick: () => setGoToSlide(index) };
   });
 
