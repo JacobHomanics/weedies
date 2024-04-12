0f86741b25c9f47a31646aa3bc45bd1a46b77c4e
Jacob Homanics <homanicsjake@gmail.com>
2024-04-12 03:05:54 -0500

    latest

--- packages/nextjs/utils/scaffold-eth/getMetadata.ts
+++ packages/nextjs/utils/scaffold-eth/getMetadata.ts
@@ -9,7 +9,7 @@ export const getMetadata = ({
   description: string;
   imageRelativePath?: string;
 }): Metadata => {
-  const baseUrl = "https://www.weedies-nextjs.vercel.app/";
+  const baseUrl = "https://weedies-nextjs.vercel.app/";
   // ? `https://${process.env.VERCEL_URL}`
   // : `http://localhost:${process.env.PORT || 3000}`;
   const imageUrl = `${baseUrl}${imageRelativePath}`;
